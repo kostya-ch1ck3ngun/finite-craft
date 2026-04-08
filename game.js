2b17077d3b2c70303e092fde6552c5408a331b3f
misterPIDARASkoncheniy <fleksermiha@gmail.com>
2026-04-08 22:38:50 +0300

    otkat

--- game.js
+++ game.js
@@ -185,7 +185,7 @@ async function parseElementsFile(file) {
         let response = await fetch(file);
         let text = await response.text();
 
-        let lines = text.split('/');
+        let lines = text.split('\r\n');
             
         for (let line of lines) {
             let words = line.split(' ');
@@ -223,7 +223,7 @@ async function parseRecipeFile(file) {
         let response = await fetch(file);
         let text = await response.text();
 
-        let lines = text.split("/");
+        let lines = text.split('\r\n');
 
         for (let line of lines) {
             let words = line.split(' ');
